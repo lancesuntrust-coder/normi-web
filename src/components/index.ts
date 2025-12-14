5cb325decab27187f2828710c6ab05c3d26cb7f3
Lance Derecho <derecholance@gmail.com>
2025-12-15 01:22:16 +0900

    Remove src/components/index.ts barrel to enforce explicit canonical imports

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./layout/TopControls";
-export * from "./ui/BottomNavPill";
-export * from "./sections/Hero";
-export * from "./ui/HeroVisual";
-export * from "./sections/ScenePreview";
-export * from "./layout/Footer";
